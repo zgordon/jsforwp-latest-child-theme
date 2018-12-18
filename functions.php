8b74c1561a8780e4f9aaf185472d637d4a56a19b
Zach Wills <zachwills@live.com>
2018-12-17 19:36:09 -0500

    Tweaks before pushing.

--- functions.php
+++ functions.php
@@ -1,17 +1,19 @@
 <?php
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
 
-if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
-  // wp_enqueue_style( 'dashicons' );
-  function chld_thm_cfg_parent_css() {
-    wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'dashicons' ) );
-    wp_enqueue_style( 'custom-style', trailingslashit( get_stylesheet_directory_uri() ) . 'custom-style.css', ['fontawesome'] );
-  }
+if ( ! function_exists( 'chld_thm_cfg_parent_css' ) ):
+	// wp_enqueue_style( 'dashicons' );
+	function chld_thm_cfg_parent_css() {
+		wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'dashicons' ) );
+		wp_enqueue_style( 'custom-style', trailingslashit( get_stylesheet_directory_uri() ) . 'custom-style.css', [ 'fontawesome' ] );
+	}
 endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
@@ -21,126 +23,128 @@ add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
  */
 
 function wistia_customizations_scripts() {
-	wp_enqueue_script( 'wistia-player', get_stylesheet_directory_uri() . '/js/wistia-player.js', array( 'jquery' ), 1.3, true);
+	wp_enqueue_script( 'wistia-player', get_stylesheet_directory_uri() . '/js/wistia-player.js', array( 'jquery' ), 1.3, true );
 
 }
+
 add_action( 'wp_enqueue_scripts', 'wistia_customizations_scripts' );
 
 
-define ( 'BP_AVATAR_THUMB_WIDTH', 40 );
+define( 'BP_AVATAR_THUMB_WIDTH', 40 );
 
-if ( function_exists('register_sidebar') ) {
-  register_sidebar(array(
-    'id' => 'learndash_sidebar',
-    'name' => 'Course Sidebar',
-    'before_widget' => '<div class="learndash-sidebar">',
-    'after_widget' => '</div>',
-    'before_title' => '<h3>',
-    'after_title' => '</h3>',
-  ));
-  register_sidebar(array(
-    'id' => 'blog_sidebar',
-    'name' => 'Blog Sidebar',
-    'before_widget' => '<div class="blog-sidebar">',
-    'after_widget' => '</div>',
-    'before_title' => '<h3>',
-    'after_title' => '</h3>',
-  ));
+if ( function_exists( 'register_sidebar' ) ) {
+	register_sidebar( array(
+		'id'            => 'learndash_sidebar',
+		'name'          => 'Course Sidebar',
+		'before_widget' => '<div class="learndash-sidebar">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3>',
+		'after_title'   => '</h3>',
+	) );
+	register_sidebar( array(
+		'id'            => 'blog_sidebar',
+		'name'          => 'Blog Sidebar',
+		'before_widget' => '<div class="blog-sidebar">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3>',
+		'after_title'   => '</h3>',
+	) );
 
 }
 
 
-add_filter ( 'woocommerce_account_menu_items', 'misha_sort_menu' );
-function misha_sort_menu( $menu_links ){
- 
+add_filter( 'woocommerce_account_menu_items', 'misha_sort_menu' );
+function misha_sort_menu( $menu_links ) {
+
 	$menu_links = array(
-		'edit-account' => 'My Account',
-    'orders' => 'Orders',
-    'subscriptions' => 'Subscriptions',
-		'edit-address' => 'Addresses',
+		'edit-account'    => 'My Account',
+		'orders'          => 'Orders',
+		'subscriptions'   => 'Subscriptions',
+		'edit-address'    => 'Addresses',
 		'payment-methods' => 'Payment Methods',
-		'vat-number' => 'VAT Number',
-  );
- 
+		'vat-number'      => 'VAT Number',
+	);
+
 	return $menu_links;
 
 }
 
 
-add_filter ( 'woocommerce_account_menu_items', 'misha_one_more_link' );
-function misha_one_more_link( $menu_links ){
- 
+add_filter( 'woocommerce_account_menu_items', 'misha_one_more_link' );
+function misha_one_more_link( $menu_links ) {
+
 	// we will hook "anyuniquetext123" later
 	$new = array( 'anyuniquetext123' => 'Course Dashboard' );
- 
+
 	// or in case you need 2 links
 	// $new = array( 'link1' => 'Link 1', 'link2' => 'Link 2' );
- 
+
 	// array_slice() is good when you want to add an element between the other ones
-  $menu_links = array_slice( $menu_links, 0, 1, true ) 
-  + $new 
-	+ array_slice( $menu_links, 0, NULL, true );
-  
+	$menu_links = array_slice( $menu_links, 0, 1, true )
+	              + $new
+	              + array_slice( $menu_links, 0, null, true );
+
 	return $menu_links;
- 
- 
+
+
 }
- 
+
 add_filter( 'woocommerce_get_endpoint_url', 'misha_hook_endpoint', 10, 4 );
-function misha_hook_endpoint( $url, $endpoint, $value, $permalink ){
- 
-	if( $endpoint === 'anyuniquetext123' ) {
- 
+function misha_hook_endpoint( $url, $endpoint, $value, $permalink ) {
+
+	if ( $endpoint === 'anyuniquetext123' ) {
+
 		// ok, here is the place for your custom URL, it could be external
 		$url = 'https://javascriptforwp.com/dashboard/';
- 
+
 	}
+
 	return $url;
- 
+
 }
 
 add_filter( 'body_class', 'bbloomer_wc_product_cats_css_body_class' );
- 
-function bbloomer_wc_product_cats_css_body_class( $classes ){
-  if( is_singular( 'product' ) )
-  {
-    $custom_terms = get_the_terms(0, 'product_cat');
-    if ($custom_terms) {
-      foreach ($custom_terms as $custom_term) {
-        $classes[] = 'product_cat_' . $custom_term->slug;
-      }
-    }
-  }
-  return $classes;
+
+function bbloomer_wc_product_cats_css_body_class( $classes ) {
+	if ( is_singular( 'product' ) ) {
+		$custom_terms = get_the_terms( 0, 'product_cat' );
+		if ( $custom_terms ) {
+			foreach ( $custom_terms as $custom_term ) {
+				$classes[] = 'product_cat_' . $custom_term->slug;
+			}
+		}
+	}
+
+	return $classes;
 }
 
-add_action( 'woocommerce_after_shop_loop_item', 'user_logged_in_product_already_bought', 30 );
-add_action( 'woocommerce_before_single_product', 'user_logged_in_product_already_bought', 30 );
- 
-function user_logged_in_product_already_bought() {
-  if ( is_user_logged_in() ) {
-    global $product;
-    $product_cats_ids = wc_get_product_term_ids( $product->get_id(), 'product_cat' );
-    $current_user = wp_get_current_user();
-    if ( wc_customer_bought_product( $current_user->user_email, $current_user->ID, $product->get_id() ) && in_array('760', $product_cats_ids)) {
-      echo '<div class="user-bought">You should have access to this course in your <a href="/dashboard">Dashboard</a></div>';
-    }
-  }
-}
-
-if ( (isset($_GET['action']) && $_GET['action'] != 'logout') || (isset($_POST['login_location']) && !empty($_POST['login_location'])) ) {
-  add_filter('login_redirect', 'my_login_redirect', 10, 3);
-  function my_login_redirect() {
-          $location = $_SERVER['HTTP_REFERER'];
-          wp_safe_redirect($location);
-          exit();
-  }
+if ( ( isset( $_GET['action'] ) && $_GET['action'] != 'logout' ) || ( isset( $_POST['login_location'] ) && ! empty( $_POST['login_location'] ) ) ) {
+	add_filter( 'login_redirect', 'my_login_redirect', 10, 3 );
+	function my_login_redirect() {
+		$location = $_SERVER['HTTP_REFERER'];
+		wp_safe_redirect( $location );
+		exit();
+	}
 }
 
 // END ENQUEUE PARENT ACTION
 
+function jfw_show_course_message_if_user_has_access() {
+	if ( ! is_user_logged_in() ) {
+		return;
+	}
+
+	if ( ! jfw_user_can_access_course() ) {
+		return;
+	}
+
+	echo '<div class="user-bought">You have access to this course. Click <a href="' . esc_url( jfw_get_course_link() ) . '">here</a> to go to the course.</div>';
+}
+
+add_action( 'woocommerce_before_single_product', 'jfw_show_course_message_if_user_has_access', 30 );
+
 function jfw_user_can_access_course() {
-	$has_access = false;
+	$has_access      = false;
 	$product_courses = get_post_meta( get_the_ID(), '_related_course', true );
 	if ( empty( $product_courses ) || empty( $product_courses[0] ) ) {
 		return $has_access;
@@ -156,6 +160,16 @@ function jfw_user_can_access_course() {
 	return $has_access;
 }
 
+function jfw_update_product_link_to_go_to_course( $link ) {
+	if ( jfw_user_can_access_course() ) {
+		return jfw_get_course_link();
+	}
+
+	return $link;
+}
+
+add_filter( 'woocommerce_loop_product_link', 'jfw_update_product_link_to_go_to_course' );
+
 function jfw_maybe_remove_cart_link( $link_html ) {
 	if ( jfw_user_can_access_course() ) {
 		return '';
@@ -163,10 +177,11 @@ function jfw_maybe_remove_cart_link( $link_html ) {
 
 	return $link_html;
 }
+
 add_filter( 'woocommerce_loop_add_to_cart_link', 'jfw_maybe_remove_cart_link' );
 
 function jfw_get_course_link() {
-	$url = get_the_permalink();
+	$url             = get_the_permalink();
 	$product_courses = get_post_meta( get_the_ID(), '_related_course', true );
 	if ( ! empty( (array) $product_courses[0] ) ) {
 		$url = get_the_permalink( $product_courses[0] );
@@ -174,3 +189,16 @@ function jfw_get_course_link() {
 
 	return $url;
 }
+
+function jfw_hide_annual_membership_from_courses_page( $query ) {
+	$user                     = get_userdata( get_current_user_id() );
+	$annual_member_product_id = 47866;
+	if ( wc_customer_bought_product( $user->user_email, $user->ID, $annual_member_product_id ) ) {
+		$query->set( 'post__not_in', array( $annual_member_product_id ) );
+	}
+
+	return $query;
+
+}
+
+add_action( 'woocommerce_product_query', 'jfw_hide_annual_membership_from_courses_page' );
--- woocommerce/price.php
+++ woocommerce/price.php
@@ -1,19 +1,17 @@
 <?php
 /**
  * Loop Price
- *
  * This template can be overridden by copying it to yourtheme/woocommerce/loop/price.php.
- *
  * HOWEVER, on occasion WooCommerce will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
  * maintain compatibility. We try to do this as little as possible, but it does
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     1.6.4
+ * @see           https://docs.woocommerce.com/document/template-structure/
+ * @author        WooThemes
+ * @package       WooCommerce/Templates
+ * @version       1.6.4
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -23,10 +21,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $product;
 ?>
 
-<?php if ( jfw_user_can_access_course() ) :	?>
+<?php if ( jfw_user_can_access_course() ) : ?>
 	<a href="<?php echo esc_url( jfw_get_course_link() ); ?>" class="button">
 		Go to Course
 	</a>
 <?php elseif ( $price_html = $product->get_price_html() ) : ?>
-		<span class="price"><?php echo $price_html; ?></span>
+	<span class="price"><?php echo $price_html; ?></span>
 <?php endif;
